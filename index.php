35b0646bd6b9a0220f5a2752adf0e8b51a667cc7
Blake Helms <blake@blakehelms.net>
2019-02-22 16:56:15 -0600

    Added comments back to the theme

--- index.php
+++ index.php
@@ -144,6 +144,12 @@
 					</article>
 
 				<?php endwhile; ?>
+<?php
+							// If comments are open or we have at least one comment, load up the comment template
+		 					if ( comments_open() || '0' != get_comments_number() )
+												comments_template( '', true );
+		 															?>
+		
 
 			<?php else : ?>
 				
